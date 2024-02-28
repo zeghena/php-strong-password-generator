c0ff77cb7be0f713f2f77f4f4a33684079404dbb
davidemarchini <marchini73@gmail.com>
2024-02-28 16:33:05 +0100

    new form

--- index.php
+++ index.php
@@ -10,6 +10,18 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
+
+    <div class="container mt-5">
+        <h2>Crea la tua nuova Password:</h2>
+        <form method="GET" class="row">
+            <div class="col-10" >
+            <input" type="text" class="form-control" name="password">
+            </div>
+            <div class="col-2" >
+                <button class="btn btn-success">Genera Password</button>
+            </div>
+        </form>
+    </div>
     
 </body>
 </html>
